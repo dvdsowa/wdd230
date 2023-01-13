9900e9fbe6149dbbb7c79e7acb985d5f217e7a3f
dvdsowa <sow11003@byui.edu>
2023-01-12 22:04:45 -0500

    team assignment w2

---
+++ scripts/darkMode.js
@@ -0,0 +1,23 @@
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
+const Section1 = document.querySelector("#learning-activities");
+const Section2 = document.querySelector("#project");
+const Section3 = document.querySelector("#information");
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("☑️")) {
+		main.style.background = "#000";
+		main.style.color = "#fff";
+		Section1.style.background = "#000";
+		Section2.style.background = "#000";
+		Section3.style.background = "#000";
+		modeButton.textContent = "❎ Toggle light mode";
+	} else {
+		main.style.background = "#eee";
+		main.style.color = "#000";
+		Section1.style.background = "#eeeeee";
+		Section2.style.background = "#eeeeee";
+		Section3.style.background = "#eeeeee";
+		modeButton.textContent = "☑️ Toggle dark mode";
+	}
+});
