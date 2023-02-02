86e0d49cdfd46ed474dbe00d10bd5972bc5e716a
dvdsowa <sow11003@byui.edu>
2023-02-02 17:32:15 -0500

    l

---
+++ scripts/makecards.js
@@ -0,0 +1,5 @@
+const githuburl = "//dvdsowa.github.io/wdd230/"
+
+fetch('//dvdsowa.github.io/wdd230/scripts/learning-activities.json')
+    .then((results) => results)
+    .then((data) => console.log(data));
---
+++ scripts/weather.js
@@ -0,0 +1,32 @@
+// select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+const url = '//api.openweathermap.org/data/2.5/weather?q=Simsbury&units=imperial&appid=92a60fb4b1b63b23d2314a50d145deae';
+
+async function apiFetch() {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        // console.log(data); // testing only
+        displayResults(data); // uncomment when ready
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+  }
+  
+  function displayResults(data) {
+  currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+  const iconsrc = `//openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+  let alter = data.weather[0].main;
+  let desc = data.weather[0].description;
+  weatherIcon.setAttribute('src', iconsrc);
+  weatherIcon.setAttribute('alt', alter);
+  captionDesc.textContent = `${desc}`;
+}
+
+  apiFetch();
