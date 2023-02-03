dd88c868e47b51eebc828afe21c3dabf8ae73e29
dvdsowa <sow11003@byui.edu>
2023-02-02 19:49:04 -0500

    update

--- scripts/makecards.js
+++ scripts/makecards.js
@@ -1,19 +1,18 @@
-const url = "https://dvdsowa.github.io/wdd230/scripts/learning-activities.json"
 const cards = document.querySelector('#cards');
 
 async function getLinkData() {
-    const response = await fetch(url);
+    const response = await fetch("https://dvdsowa.github.io/wdd230/scripts/learning-activities.json");
     const data = await response.json();
     keys = Object.keys(data);
-    displayLinkData(data.keys); 
+    displayLinkData(data,keys); 
 }
 
 function displayLinkData(data,keys) {
     keys.forEach((key) => {
-        let li = document.createElement('li');  
+        const li = document.createElement('li');  
         li.textContent = key + ": ";
         data[key].forEach(activity => {
-            let a = document.createElement('a');
+            const a = document.createElement('a');
             a.setAttribute('href', activity.link);
             a.setAttribute('target', '_blank');
             a.textContent = activity.name + ' | ';;
