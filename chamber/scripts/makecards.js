125a17ca87e2baff3d578dbbaeaf35ff12332110
dvdsowa <sow11003@byui.edu>
2023-02-04 20:37:26 -0500

    f

--- chamber/scripts/makecards.js
+++ chamber/scripts/makecards.js
@@ -9,14 +9,31 @@ async function getLinkData() {
 
 function displayLinkData(data,keys) {
     keys.forEach((key) => {
-        const li = document.createElement('li');  
-        li.textContent = key + ": ";
+        const li = document.createElement('li');
         data[key].forEach(business => {
+            const span = document.createElement('span');
             const p = document.createElement('p');
+            const p2 = document.createElement('p');
+            const p3 = document.createElement('p');
+            const p4 = document.createElement('p');
+            const p5 = document.createElement('p');
+            const p6 = document.createElement('p');
             //a.setAttribute('href', activity.link);
             //a.setAttribute('target', '_blank');
-            p.textContent = `${business.name}; ${business.address}; ${business.phonenumber}; ${business.url}; ${business.image}; ${business.membershiplevel}; ${business.other}`;;
+            span.textContent = `${business.name}`;
+            p.textContent =  `Address: ${business.address}`;
+            p2.textContent = `Phone Number: ${business.phonenumber}`;
+            p3.textContent = `Website: ${business.url}`;
+            p4.textContent = `Logo: ${business.image}`;
+            p5.textContent = `Membership Level: ${business.membershiplevel}`;
+            p6.textContent = `Other: ${business.other}`;
+            li.appendChild(span);
             li.appendChild(p);
+            li.appendChild(p2);
+            li.appendChild(p3);
+            li.appendChild(p4);
+            li.appendChild(p5);
+            li.appendChild(p6);
         });  
         cards.appendChild(li);
     });
